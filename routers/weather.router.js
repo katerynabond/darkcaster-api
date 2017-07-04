57e89a485063280a444632d22e1b224cce565cb8
Kateryna Bondareva <kbondareva@gmail.com>
2017-07-04 15:11:44 -0400

    Added the ability to look weather by location

--- routers/weather.router.js
+++ routers/weather.router.js
@@ -1,8 +1,9 @@
-
+/*jshint esversion: 6*/
 const express = require ('express');
 const router = express.Router();
 const darksky = process.env.DARKSKY || require('../credentials').darksky;
 const baseUrl = `https://api.darksky.net/forecast/${darksky}/`;
+const baseUrlMaps = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
 const axios = require('axios');
 
 router.get('/weather', (request, response, next) => {
@@ -31,4 +32,28 @@ router.get('/weather/:lat,:lon', (request, response, next) => {
        });
 
 });
+
+
+
+router.get('/weather/location/:location', (request, response, next) => {
+  const location = request.params.location;
+  const urlMaps = `${baseUrlMaps}${location}`;
+
+  axios.get(urlMaps)
+       .then(loc => {
+         const lat = loc.data.results[0].geometry.location.lat;
+         const lon = loc.data.results[0].geometry.location.lng;
+         const url = `${baseUrl}${lat},${lon}`;
+
+         axios.get(url)
+              .then(weather => {
+               response.json(weather.data);
+             });
+
+     })
+     .catch(err => {
+       next(err);
+     });
+});
+
 module.exports = router;
